c195cdc1b19b1db6d80388ad50cb45e927bc0394
Daniel <daka98001@gmail.com>
2025-01-14 11:36:30 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/StringExercises.java
@@ -0,0 +1,16 @@
+package se.lexicon;
+
+public class StringExercises {
+
+    public static void main(String[] args) {
+
+        ex1();
+    }
+
+    public static void ex1(){
+        String java = "Java"; // 4
+        System.out.println(java.length());
+        String sentence = "Long example sentence";
+        System.out.println(sentence.indexOf('o'));
+    }
+}
